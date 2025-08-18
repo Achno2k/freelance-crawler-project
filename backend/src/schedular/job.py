478bb6160705e29a09187afedeb4f86889469b58
Achno2k <amansinghh525@gmail.com>
2025-08-18 23:18:29 +0530

    fix

--- backend/src/schedular/job.py
+++
@@ -1,8 +0,0 @@
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from cache.utils import set_results
-
-schedular = AsyncIOScheduler()
-
-def start_job():
-    schedular.add_job(set_results, "interval", hours=24)
-    schedular.start()
